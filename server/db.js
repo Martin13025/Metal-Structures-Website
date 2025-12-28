a51c3d5a787e3703b2e6cd86b6cefef322f5cfe5
Martin <115161917+Martin13025@users.noreply.github.com>
2025-12-27 22:22:45 -0300

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,9 +2,9 @@ const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
   host: "localhost",
-  user: "root", // твой MySQL пользователь
-  password: "", // пароль
-  database: "metal_site", // имя базы, которую создал
+  user: "root",
+  password: "",
+  database: "metal_site",
 });
 
 module.exports = db;
